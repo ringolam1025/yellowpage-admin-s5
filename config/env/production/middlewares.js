08c9c1b5c8fc1a1bca865180766abc539dcb49bf
Ringo <ringoching93@gmail.com>
2026-01-25 02:28:39 -0700

    Update middlewares

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,44 +1,47 @@
-const r2Host = (env("R2_PUBLIC_URL") || "").replace(/^https?:\/\//, "");
+module.exports = ({ env }) => {
+  const r2PublicUrl = env("R2_PUBLIC_URL", "") || "";
+  const r2Host = r2PublicUrl.replace(/^https?:\/\//, "");
 
-module.exports = ({ env }) => [
-  "strapi::logger",
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "*.cloudflarecounts.com",
-            r2Host,
-          ],
-          "media-src": ["'self'", "data:", "blob:", r2Host],
-          upgradeInsecureRequests: null,
+  return [
+    "strapi::logger",
+    "strapi::errors",
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+            "img-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              "dl.airtable.com",
+              "*.cloudflarecounts.com",
+              r2Host,
+            ],
+            "media-src": ["'self'", "data:", "blob:", r2Host],
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  {
-    name: "strapi::body",
-    config: {
-      formLimit: "256mb", // modify form body
-      jsonLimit: "256mb", // modify JSON body
-      textLimit: "256mb", // modify text body
-      formidable: {
-        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::query",
+    {
+      name: "strapi::body",
+      config: {
+        formLimit: "256mb", // modify form body
+        jsonLimit: "256mb", // modify JSON body
+        textLimit: "256mb", // modify text body
+        formidable: {
+          maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+        },
       },
     },
-  },
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ];
+};
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,34 +1,37 @@
-const r2Host = (env("R2_PUBLIC_URL") || "").replace(/^https?:\/\//, "");
+module.exports = ({ env }) => {
+  const r2PublicUrl = env("R2_PUBLIC_URL", "") || "";
+  const r2Host = r2PublicUrl.replace(/^https?:\/\//, "");
 
-module.exports = ({ env }) => [
-  "strapi::logger",
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "*.cloudflarecounts.com",
-            r2Host,
-          ],
-          "media-src": ["'self'", "data:", "blob:", r2Host],
-          upgradeInsecureRequests: null,
+  return [
+    "strapi::logger",
+    "strapi::errors",
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+            "img-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              "dl.airtable.com",
+              "*.cloudflarecounts.com",
+              r2Host,
+            ],
+            "media-src": ["'self'", "data:", "blob:", r2Host],
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ];
+};
